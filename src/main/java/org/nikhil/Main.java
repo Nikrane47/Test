e5f1bbaa2853a1ba283c9ec9784d3f234d27cb1f
Nikrane47 <nikrane47@gmail.com>
2024-02-11 00:25:20 +0530

    Test

--- src/main/java/org/nikhil/Main.java
+++ src/main/java/org/nikhil/Main.java
@@ -3,5 +3,5 @@ package org.nikhil;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        System.out.println("Hello world!");}
 }
